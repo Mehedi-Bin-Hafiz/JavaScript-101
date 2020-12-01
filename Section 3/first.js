a4dbf0d0c13febdd7a6a3ff7fffc312affd1a6c7
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-12-01 08:06:01 +0600

    practice

---
+++ Section 3/first.js
@@ -0,0 +1,14 @@
+
+var teacher_name = 'Mehedi hasan'
+
+if( teacher_name == 'Mehedi hasan'){
+    let apples = 5 
+    const height = "6'0\""
+    console.log('let and const usable only in this function')
+    console.log('I have', apples, "apples")
+}
+
+// console.log('I have', apples, "apples")
+
+
+
