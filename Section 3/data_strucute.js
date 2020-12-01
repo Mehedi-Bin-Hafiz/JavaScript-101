d4dfc821129ed33ec6cbc5ad890965acd9c526eb
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-12-01 08:05:22 +0600

    intro to data strucutre

---
+++ Section 3/data_strucute.js
@@ -0,0 +1,21 @@
+const str = 'Mehedi Hasan'
+const num = 3
+const flt = 3.34
+const bool = true // false
+const list = ['mehedi ', ' is ', '1']
+console.log(list);
+const thig = null ; // represent nothing
+const notdefined = undefined;
+
+var getAge = function(){
+    return 32;
+
+}
+console.log(getAge);
+const obj ={
+    'name': "mehedi hasan",
+    "age": 23
+
+}
+
+console.log(obj.name)
