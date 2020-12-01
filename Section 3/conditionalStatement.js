9f561f4783108d85c913b3890653d5d594245fa3
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-12-01 08:28:08 +0600

    conditional statement

---
+++ Section 3/conditionalStatement.js
@@ -0,0 +1,14 @@
+const twitter = '@mehedibin'
+
+if (twitter == '@mehedibinhafiz'){
+    // exexute this code
+    console.log('thank you so much');
+
+} 
+else if( twitter == '@mehedibin'){
+    console.log('i am mehedibin');
+}
+else{
+    console.log('Sorry this is not my account');
+}
+
